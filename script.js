2283bb4e704e10ff0857281247618f6f9a322ca4
danie11z <daniel.estudando1@gmail.com>
2022-06-29 06:43:07 -0300

    Activity completed.

---
+++ script.js
@@ -0,0 +1,59 @@
+let comedy = [{
+    title: "O Auto da Compadecida",
+    director: "Guel Arraes",
+    year: 2000,
+},
+{
+    title: "Minha Mãe é Uma Peça 3",
+    director: "Susana Garcia",
+    year: 2019,
+},
+{
+    title: "Tô Ryca!",
+    director: "Pedro Antonio",
+    year: 2016,
+},]
+
+console.log(comedy)
+
+let action = [{
+    title: "Batman - O Cavaleiro Das Trevas",
+    director: "Christopher Nolan",
+    year: 2008,
+},{
+    title: "Pantera Negra",
+    director: "Ryan Coogler",
+    year: 2018,
+},{
+    title: "Invasão Zumbi",
+    director: "Yeong Sang-ho",
+    year: 2016,
+},]
+
+console.log(action)
+
+let fantasy = [{
+    title: "Moana",
+    director: "Ron Clements, John Musker",
+    year: 2017,
+},{
+    title: "Soul",
+    director: "Pete Docter",
+    year: 2020,
+},{
+    title: "Steven Universo: O Filme",
+    director: "Rebecca Sugar",
+    year: 2019,
+},]
+
+console.log(fantasy)
+
+let cartoon = "Pica-Pau"
+
+if(cartoon == "Pica-Pau"){
+    console.log("Obra prima!")
+}else if(cartoon == "O Novo Pica-Pau"){
+    console.log("Por favor, desligue a televisão.")
+}else{
+    console.log("Desde que não seja O Novo Pica-Pau.")
+}
